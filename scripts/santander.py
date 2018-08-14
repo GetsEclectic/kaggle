82542f039674d3f180351ecfa9560eb9767b2d2f
Ben Solwitz <ben.solwitz@nytimes.com>
2018-08-14 12:53:19 -0400

    deleted unused code and optimized imports

--- scripts/santander.py
+++ scripts/santander.py
@@ -3,8 +3,8 @@
 # For example, here's several helpful packages to load in
 
 import lightgbm as lgb
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 import sklearn.ensemble as ensemble
 import sklearn.gaussian_process as gaussian_process
 import sklearn.linear_model as linear_model
@@ -14,10 +14,7 @@ import sklearn.tree as tree
 from scipy.stats import spearmanr
 from sklearn import model_selection
 from sklearn.metrics import mean_squared_error, make_scorer
-from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import train_test_split
 from skopt import BayesSearchCV
 from xgboost import XGBRegressor
 
@@ -50,23 +47,6 @@ def create_submission_file(my_pipeline, features):
     submission.to_csv('submission.csv', index=False)
 
 
-def rmse_cv(model,X,y):
-    rmse = np.sqrt(-cross_val_score(model, X, y, scoring="neg_mean_squared_error", cv=5))
-    return rmse
-
-
-class grid():
-    def __init__(self,model):
-        self.model = model
-
-    def grid_get(self,X,y,param_grid):
-        grid_search = GridSearchCV(self.model,param_grid,cv=5, scoring="neg_mean_squared_log_error")
-        grid_search.fit(X,y)
-        print(grid_search.best_params_, np.sqrt(-grid_search.best_score_))
-        grid_search.cv_results_['mean_test_score'] = np.sqrt(-grid_search.cv_results_['mean_test_score'])
-        print(pd.DataFrame(grid_search.cv_results_)[['params','mean_test_score','std_test_score']])
-
-
 def drop_constant_columns(X):
     unique_df = X.nunique().reset_index()
     unique_df.columns = ["col_name", "unique_count"]
@@ -86,71 +66,6 @@ def get_high_spearman_corr_with_target(X, y):
     corr_df = corr_df.sort_values(by='corr_values')
     high_corr_df = corr_df[(corr_df['corr_values']>0.1) | (corr_df['corr_values']<-0.1)]
     return high_corr_df
-
-
-def run_lgb(train_X, train_y, val_X, val_y, test_X):
-    params = {
-        "objective" : "regression",
-        "metric" : "rmse",
-        "num_leaves" : 30,
-        "learning_rate" : 0.01,
-        "bagging_fraction" : 0.7,
-        "feature_fraction" : 0.7,
-        "bagging_frequency" : 5,
-        "bagging_seed" : 2018,
-        "verbosity" : -1
-    }
-
-    lgtrain = lgb.Dataset(train_X, label=train_y)
-    lgval = lgb.Dataset(val_X, label=val_y)
-    evals_result = {}
-    model = lgb.train(params, lgtrain, 1000, valid_sets=[lgval], early_stopping_rounds=100, verbose_eval=200, evals_result=evals_result)
-
-    pred_test_y = model.predict(test_X, num_iteration=model.best_iteration)
-    return pred_test_y, model, evals_result
-
-
-def kfold_cv(train_X, train_y, test_X):
-    kf = model_selection.KFold(n_splits=5, shuffle=True, random_state=2017)
-    pred_test_full = 0
-    for dev_index, val_index in kf.split(train_X):
-        dev_X, val_X = train_X.iloc[dev_index], train_X.iloc[val_index]
-        dev_y, val_y = train_y.iloc[dev_index], train_y.iloc[val_index]
-        pred_test, model, evals_result = run_lgb(dev_X, dev_y, val_X, val_y, test_X)
-        pred_test_full += pred_test
-    pred_test_full /= 5.
-    pred_test_full = np.expm1(pred_test_full)
-    return model, pred_test_full
-
-
-def main():
-    training_data = pd.read_csv("../input/santander-value-prediction-challenge/train.csv")
-    y = np.log1p(training_data.target)
-    X = training_data.drop(['target', 'ID'], axis=1)
-
-    cols_with_high_corr = get_high_spearman_corr_with_target(X, y).col_labels.tolist()
-
-    X_with_high_corr = X[cols_with_high_corr]
-
-    print(X_with_high_corr.shape)
-
-    train_X, test_X, train_y, test_y = train_test_split(X_with_high_corr, y, test_size=0.25)
-
-    model, pred_test_full = kfold_cv(train_X, train_y, test_X)
-
-    print(pred_test_full)
-
-    # my_pipeline = Pipeline([('scaler', RobustScaler()), ('rg', RandomForestRegressor(n_estimators=10))])
-    #
-    # my_pipeline.fit(train_X, train_y)
-    #
-    # preds = my_pipeline.predict(test_X)
-    #
-    # describe(preds)
-    #
-    # print("training RMSLE: " + str(sqrt(mean_squared_log_error(np.expm1(test_y), np.expm1(preds)))))
-
-    # create_submission_file(my_pipeline)
 
 
 def rmse(y_true, y_pred):
@@ -462,9 +377,3 @@ def model_comparison():
 
 
 xgb_with_important_features()
-
-# adding deskew took forever, aborted it
-# RandomForestRegressor working better than XGBRegressor
-# PCA made it much worse and took a long time
-# try ensembling and/or stacking?
-# grid search took forever
